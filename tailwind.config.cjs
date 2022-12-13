eed8c221f4baf0b98ac47d81edde3f7b360bbfeb
yaikohi <erik.beem@hotmail.com>
2022-12-13 14:18:00 +0100

    Updated prose styling, tweaked a bit, remove templating

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            "p,h1,h2,h3,h4,h5,h6,ul,li,code,ol,th,tr,strong": {
+              color: "rgb(223, 223, 223)",
+            },
+            mark: {
+              color: "black",
+              backgroundColor: "rgb(223, 223, 223)",
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
