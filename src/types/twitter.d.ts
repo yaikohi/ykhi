b189a3628eb17286ab604fb39ae1a9f8cc0fdc8d
yaikohi <erik.beem@hotmail.com>
2022-12-15 15:18:30 +0100

    Styled list of tweets  ver. 1

---
+++ src/types/twitter.d.ts
@@ -0,0 +1,38 @@
+export interface ITweetObj {
+  id: string;
+  created_at: string;
+  author_id: string;
+  attachments: { media_keys: string[] };
+  text: string;
+  edit_history_tweet_ids: string[];
+  media?: IMediaObj[];
+}
+
+export interface IMediaObj {
+  url: string;
+  height: number;
+  type: TMediaType;
+  media_key: string;
+  width: number;
+  preview_image_url?: string;
+}
+
+export interface IUserObj {
+  protected: boolean;
+  id: string;
+  public_metrics: IPublicMetrics;
+  username: string;
+  name: string;
+  verified: boolean;
+  url: string;
+  tweets: ITweetObj[];
+}
+
+export interface IPublicMetrics {
+  followers_count: number;
+  following_count: number;
+  tweet_count: number;
+  listed_count: number;
+}
+
+export type TMediaType = "photo" | "video";
