31cf578a049840f876e89b5f1d41f6260f6b68c9
yaikohi <erik.beem@hotmail.com>
2023-01-03 19:13:35 +0100

    remove boilerplate, add about section in japanese, installed two new fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,7 +16,19 @@ module.exports = {
           },
         },
       },
+      fontFamily: {
+        dotGothic16: ["DotGothic16", "sans-serif"],
+        "social-reg": ["Social Regular", "sans-serif"],
+        "social-light": ["Social Light", "sans-serif"],
+        "social-thin": ["Social Thin", "sans-serif"],
+        "social-medium": ["Social Medium", "sans-serif"],
+        "social-black": ["Social Black", "sans-serif"],
+        "social-hairling": ["Social Hairling", "sans-serif"],
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
+  corePlugins: {
+    fontFamily: true,
+  },
 };
